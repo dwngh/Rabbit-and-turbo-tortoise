b55b9e4140bd685dd0b38617940487b27363fb28
Huy <wanghcr11@gmail.com>
2023-04-24 23:33:29 +0700

    Init protocol

--- tortoise/src/main/java/com/tortoise/Tortoise.java
+++ tortoise/src/main/java/com/tortoise/Tortoise.java
@@ -1,10 +1,6 @@
 package com.tortoise;
 
 import com.tortoise.component.Node;
-import com.tortoise.component.Sensor;
-import com.tortoise.component.Monitor;
-import com.tortoise.ui.MonitorDashboard;
-import com.tortoise.ui.SensorManager;
 
 public class Tortoise {
     public final static String HOST = "localhost";
@@ -16,21 +12,7 @@ public class Tortoise {
     public final static int TIME_WINDOW = 300;
  
     public static void main(String[] argv) throws Exception {
-        MonitorDashboard md = new MonitorDashboard();
-        SensorManager sm = new SensorManager();
-        Node s = new Sensor();
-        // ((Sensor) s).disableCompression();
-        s.start();
-        // Node s1 = new Sensor();
-        // s1.start();
-        // Node s2 = new Sensor();
-        // s2.start();
-        sm.bind((Sensor) s);
-        // sm.bind((Sensor) s1);
-        // sm.bind((Sensor) s2);
-        Monitor s3 = new Monitor();
-        s3.start();
-        s3.setMonitorDashboard(md);
+    
     }
  
 }
